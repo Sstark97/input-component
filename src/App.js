00403a31129a681b2c9af906eea4f674f4ac4c77
Sstark97 <aitorscinfo@gmail.com>
2021-08-15 19:48:06 +0100

    Fix Input component rows propierties

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <Input text="fsdf" startIcon="battery_20" helperText="911" size="md" />
+      <Input text="fsdf" startIcon="battery_20" helperText="911" row={3} />
     </div>
   );
 }
